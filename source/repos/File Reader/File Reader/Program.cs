36c763fe634ca0920742fef7f0f02f4b4f9e6410
yorliabdulai <yorliabdulai1@gmail.com>
2024-02-17 15:40:30 +0000

    Final commit

---
+++ source/repos/File Reader/File Reader/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try {
+            Console.Write("Enter the file path:");
+            string filePath = Console.ReadLine();
+            if (File.Exists(filePath))
+            {
+                string fileContents = File.ReadAllText(filePath);
+                Console.WriteLine("\nFileContents: \n");
+                Console.WriteLine(fileContents);
+            }else
+            {
+                Console.WriteLine($"File not found at the specified path: {filePath}");
+            }
+        }catch (Exception ex)
+        {
+            Console.WriteLine($"An error occured: {ex.Message}");
+        }
+       
+    }
+    
+
+    
+}
